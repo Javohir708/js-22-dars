5e20568b1a09836a23dad208c6b7dc83d5076422
Javohir708 <joxauzking@gamil.com>
2024-12-11 09:23:40 +0500

    homework

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const inputGrad = document.querySelector(".form__graduation")
 const wrapperEl = document.querySelector(".wrapper")
 const createBtn = document.querySelector(".create__btn")
 
+let users = JSON.parse(localStorage.getItem("data")) || [];
 
 function addShow() {
     popupEl.style.display = 'block'
@@ -17,29 +18,53 @@ function closeShow() {
     popupEl.style.transition = '.5s'
 }
 
-formEl.addEventListener ("submit", (event) => {
-    event.preventDefault()
-    
-    const cardEl = document.createElement('div')
-    cardEl.className = 'card'
+function createCard(user) {
+    const cardEl = document.createElement("div");
+    cardEl.className = "card";
 
     cardEl.innerHTML = `
-       <div class="image">
+        <div class="image">
             <img src="./assets/image.png" alt="">
         </div>
-           <h3>${inputName.value}</h3>
-           <p>${inputWork.value}</p>
-           <p>${inputGrad.value}</p>
-           <div class="icon">
-                <i class="fa-brands fa-instagram" style="color: #000000;"></i>
-                <i class="fa-brands fa-twitter" style="color: #000000;"></i>
-                <i class="fa-brands fa-facebook-f" style="color: #000000;"></i>
-            </div>
-            <button class="contact__btn">Contact</button> 
-    `
+        <h3>${user.name}</h3>
+        <p>${user.work}</p>
+        <p>${user.graduation}</p>
+        <div class="icon">
+            <i class="fa-brands fa-instagram" style="color: #000000;"></i>
+            <i class="fa-brands fa-twitter" style="color: #000000;"></i>
+            <i class="fa-brands fa-facebook-f" style="color: #000000;"></i>
+        </div>
+        <button class="contact__btn">Contact</button> 
+    `;
+    wrapperEl.appendChild(cardEl);
+}
 
-    wrapperEl.appendChild(cardEl)
-    inputName.value = ''
-    inputWork.value = ''
-    inputGrad.value = ''
-})
+function renderUsers() {
+    users.forEach(user => {
+        createCard(user);
+    });
+}
+
+formEl.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const newUser = {
+        name: inputName.value,
+        work: inputWork.value,
+        graduation: inputGrad.value
+    };
+
+    users.push(newUser);
+    localStorage.setItem("data", JSON.stringify(users));
+
+    createCard(newUser);
+
+    inputName.value = "";
+    inputWork.value = "";
+    inputGrad.value = "";
+    closeShow();
+});
+
+window.addEventListener("load", () => {
+    renderUsers();
+});
